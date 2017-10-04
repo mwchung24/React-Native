06dec0fd308647a67ed3131fbe89e8b3059b5558
Martin Chung <mwchung24@gmail.com>
2017-10-04 15:24:30 -0400

    Using state to make text blink

--- App.js
+++ App.js
@@ -3,18 +3,13 @@ import { StyleSheet, Text, View, AppRegistry, Image } from 'react-native';
 import Name from './names.js';
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    
-  }
   render() {
     let pic = {
       uri: 'https://www.contactcenterworld.com/images/company/NICE-Systems-600px-logo.jpg'
     };
     return (
       <View style={styles.container}>
-        <Name name='Martin' />
+        <Name name='Martin'></Name>
         <Name name='Hanna' />
         <Name name='Calvin' />
         <Image source={pic} style={picStyles.container}/>
--- names.js
+++ names.js
@@ -2,10 +2,22 @@ import React from 'react';
 import { StyleSheet, Text, View, AppRegistry, Image } from 'react-native';
 
 export default class Name extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {showText: true};
+
+    setInterval(() => {
+      this.setState(previousState => {
+        return { showText: !previousState.showText };
+      });
+    }, 1000);
+  }
   render() {
+    let display = this.state.showText ? 'Hello ' + this.props.name + '!' : ' ';
     return (
       <View>
-        <Text>Hello {this.props.name}!</Text>
+        <Text>{display}</Text>
       </View>
     );
   }
