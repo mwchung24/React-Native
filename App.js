d25853e310d387a91ca7e1bb86116b68677d2c3a
Martin Chung <mwchung24@gmail.com>
2017-10-14 10:43:14 -0400

    fixed misspelling

--- App.js
+++ App.js
@@ -23,50 +23,11 @@ export default class App extends React.Component {
       >
         <View style={styles.container}>
           <TextInput style={styles.input}
-            placeholder='Enter your name!'
             onChangeText={(text) => this.setState({text: text})}
           />
-          <FlatList
-            data={[
-              {key: this.state.text},
-              {key: this.state.text},
-              {key: this.state.text},
-              {key: this.state.text},
-              {key: this.state.text},
-              {key: this.state.text},
-              {key: this.state.text},
-              {key: this.state.text},
-              {key: this.state.text},
-              {key: this.state.text},
-              {key: this.state.text},
-              {key: this.state.text},
-              {key: 'Martin'},
-              {key: 'Martin'},
-              {key: 'Martin'},
-              {key: 'Martin'},
-              {key: 'Martin'},
-              {key: 'Martin'},
-              {key: 'Martin'},
-              {key: 'Martin'},
-              {key: 'Martin'},
-              {key: 'Martin'},
-              {key: 'Martin'},
-              {key: 'Martin'},
-              {key: 'Martin'},
-              {key: 'Martin'},
-              {key: 'Martin'},
-              {key: 'Martin'},
-              {key: 'Martin'},
-              {key: 'Martin'},
-              {key: 'Martin'},
-              {key: 'Martin'},
-              {key: 'Martin'},
-            ]}
-            renderItem={({item}) => <Name style={styles.firstName} name={item.key}/>}
-            />
-          <Image source={pic} style={styles.picture}/>
-          <View style={styles.box}/>
-          <Button/>
+          <View style={styles.buttons}>
+            <Button/>
+          </View>
         </View>
       </ScrollView>
     );
@@ -76,33 +37,83 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    // backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
     marginTop: 100,
     marginBottom: 100,
     // flexDirection: 'row',
   },
-  picture: {
-    width: 300,
-    height: 100,
+  buttons: {
+    flex: 1,
+    // flexDirection: 'row',
   },
-  firstName: {
-    color: 'blue',
-    fontWeight: 'bold',
-    fontSize: 30,
-    backgroundColor: 'orange',
-  },
-  secondName: {
-    color: 'red',
-    fontSize: 50,
-  },
-  box: {
-    width: 150,
-    height: 150,
-    backgroundColor: 'steelblue',
-  },
+  // picture: {
+  //   width: 300,
+  //   height: 100,
+  // },
+  // firstName: {
+  //   color: 'blue',
+  //   fontWeight: 'bold',
+  //   fontSize: 30,
+  //   backgroundColor: 'orange',
+  // },
+  // secondName: {
+  //   color: 'red',
+  //   fontSize: 50,
+  // },
+  // box: {
+  //   width: 150,
+  //   height: 150,
+  //   backgroundColor: 'steelblue',
+  // },
   input: {
     height: 40,
+    borderColor: 'Black',
+    borderWidth: 1,
+    borderRadius: 4,
+    width: 300,
+    marginBottom: 20,
   }
 });
+
+// <FlatList
+//   data={[
+//     {key: this.state.text},
+//     {key: this.state.text},
+//     {key: this.state.text},
+//     {key: this.state.text},
+//     {key: this.state.text},
+//     {key: this.state.text},
+//     {key: this.state.text},
+//     {key: this.state.text},
+//     {key: this.state.text},
+//     {key: this.state.text},
+//     {key: this.state.text},
+//     {key: this.state.text},
+//     {key: 'Martin'},
+//     {key: 'Martin'},
+//     {key: 'Martin'},
+//     {key: 'Martin'},
+//     {key: 'Martin'},
+//     {key: 'Martin'},
+//     {key: 'Martin'},
+//     {key: 'Martin'},
+//     {key: 'Martin'},
+//     {key: 'Martin'},
+//     {key: 'Martin'},
+//     {key: 'Martin'},
+//     {key: 'Martin'},
+//     {key: 'Martin'},
+//     {key: 'Martin'},
+//     {key: 'Martin'},
+//     {key: 'Martin'},
+//     {key: 'Martin'},
+//     {key: 'Martin'},
+//     {key: 'Martin'},
+//     {key: 'Martin'},
+//   ]}
+//   renderItem={({item}) => <Name style={styles.firstName} name={item.key}/>}
+//   />
+// <Image source={pic} style={styles.picture}/>
+// <View style={styles.box}/>
--- button.js
+++ button.js
@@ -2,46 +2,80 @@ import React from 'react';
 import { StyleSheet, Text, View, AppRegistry, Image, TextInput, Button, Alert, TouchableHighlight, TouchableOpacity, TouchableNativeFeedback, TouchableWithoutFeedback } from 'react-native';
 
 export default class buttonTest extends React.Component {
-  _onPressButton() {
-    Alert.alert('You pressed the button!');
+  _onPressButton () {
+    Alert.alert('you pressed the button');
   }
-
-  _onPressHighlightButton() {
-    Alert.alert('This is the highlight button!');
-  }
-
-  _onLongPressButton() {
-    Alert.alert('You pressed the button too long!');
-  }
-
   render() {
     return (
-      <View>
-        <Button
-          onPress={this._onPressButton}
-          title='Click Me!'
-          color='#841584'
-        />
-
-        <TouchableHighlight onPress={this._onPressHighlightButton} underlayColor='white'>
+      <View style={styles.container}>
+        <TouchableHighlight onPress={this._onPressButton} underlayColor='black'>
           <View style={styles.button}>
-            <Text style={styles.buttonText}>Click Me!</Text>
+            <Text style={styles.buttonText}>7</Text>
           </View>
         </TouchableHighlight>
-
+        <TouchableHighlight underlayColor='white'>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>8</Text>
+          </View>
+        </TouchableHighlight>
+        <TouchableHighlight underlayColor='white'>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>9</Text>
+          </View>
+        </TouchableHighlight>
+        <TouchableHighlight underlayColor='white'>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>4</Text>
+          </View>
+        </TouchableHighlight>
+        <TouchableHighlight underlayColor='white'>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>5</Text>
+          </View>
+        </TouchableHighlight>
+        <TouchableHighlight underlayColor='white'>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>6</Text>
+          </View>
+        </TouchableHighlight>
+        <TouchableHighlight underlayColor='white'>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>1</Text>
+          </View>
+        </TouchableHighlight>
+        <TouchableHighlight underlayColor='white'>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>2</Text>
+          </View>
+        </TouchableHighlight>
+        <TouchableHighlight underlayColor='black'>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>3</Text>
+          </View>
+        </TouchableHighlight>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
   button: {
-    backgroundColor: '#2196F3',
-    width: 260,
+    backgroundColor: 'gray',
+    border: 'black',
+    width: 70,
+    height: 70,
     alignItems: 'center',
     padding: 30,
+    borderRadius: 4,
+    borderWidth: 1,
+    borderColor: 'black',
   },
   buttonText: {
-    color: 'white',
+    color: 'black',
   }
 });
